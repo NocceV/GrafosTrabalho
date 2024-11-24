a989f42e6afa75e99e4ce2103a66a611fade83ed
Vitor Nocce <noccevitor@gmail.com>
2024-11-24 18:36:34 -0300

    Create Main

--- code/Grafo/Program.cs
+++ code/Grafo/Program.cs
@@ -10,57 +10,161 @@ namespace Grafo
     {
         static void Main(string[] args)
         {
-            Dictionary<int, Dictionary<int, int>> grafo = new Dictionary<int, Dictionary<int,int>>();
-            Console.WriteLine("Insira o vertice / -1 para sair");
-            int vertice = int.Parse(Console.ReadLine());
-            do
-            {
-                if (!grafo.TryGetValue(vertice, out Dictionary<int, int> vertices))
-                {
-                    grafo.Add(vertice, new Dictionary<int, int>());
-                }
-                else
-                {
-                    Console.WriteLine("Vertice ja existe");
-                }
-                Console.WriteLine("Insira o vertice / -1 para sair");
-                vertice = int.Parse(Console.ReadLine());
-            } while(vertice != -1);
-
-
-            Console.WriteLine("Insira qual vértice deseja inserir aresta / -1 para sair");
-            vertice = int.Parse(Console.ReadLine());
-            do
-            {
-                if (grafo.TryGetValue(vertice, out Dictionary<int, int> vertices))
-                {
-                    Console.WriteLine("Insira o vertice de destino e em seguida seu peso");
-                    int destino = int.Parse(Console.ReadLine());
-                    int peso = int.Parse(Console.ReadLine());
-                    grafo[vertice].Add(destino, peso);
-                }
-                else
-                {
-                    Console.WriteLine("Vertice não existe");
-                }
-                Console.WriteLine("Insira o vertice / -1 para sair");
-                vertice = int.Parse(Console.ReadLine());
-            } while (vertice != -1);
-
-            listaAdjacência(grafo);
-            Console.ReadLine();
-
+            Console.Clear();
+            start();
         }
-        public static void listaAdjacência(Dictionary<int, Dictionary<int, int>> grafo)
+
+        public static string menuInicial()
         {
-            foreach (var vertice in grafo)
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Crie um grafo");
+            stringBuilder.AppendLine("1) Criar grafo");
+            stringBuilder.AppendLine("0) Sair");
+            return stringBuilder.ToString();
+        }
+
+        public static void start()
+        {
+            int codigo;
+            do
             {
-                Console.WriteLine("Vertice: " + vertice.Key);
-                foreach (var aresta in vertice.Value)
+                try
                 {
-                    Console.WriteLine("Aresta: " + aresta.Key + " Peso: " + aresta.Value);
+                    Console.WriteLine(menuInicial());
+                    codigo = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    switch (codigo)
+                    {
+                        case 1:
+                            criarGrafo();
+                            break;
+                        case 0:
+                            Console.WriteLine("Adeus!");
+                            break;
+                        default:
+                            Console.WriteLine("Opção inválida!");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opção inválida!");
+                    codigo = -1;
+                }
+            } while (codigo != 0);
+        }
+
+
+
+        public static void criarGrafo()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            Console.WriteLine("Informe a quantidade de vertices");
+            int numVertices = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade de arestas");
+            int numArestas = int.Parse(Console.ReadLine());
+
+            List<(int origem, int destino, double peso)> pesoArestas = new List<(int, int, double)>();
+
+            for (int i = 1; i <= numArestas; i++)
+            {
+                Console.WriteLine($"Informe o vértice de origem da aresta {i}:");
+                int verticeOrigem = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Informe o vértice de destino da aresta {i}:");
+                int verticeDestino = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Informe o peso da aresta {i}:");
+                double peso = double.Parse(Console.ReadLine());
+
+
+                pesoArestas.Add((verticeOrigem, verticeDestino, peso));
+            }
+
+
+            Grafo grafo = new Grafo(numVertices, numArestas, pesoArestas);
+            Console.Clear();
+            Console.WriteLine("Grafo criado");
+            menuGrafo(grafo);
+        }
+
+        public static void menuGrafo(Grafo grafo)
+        {
+            bool repetidor = true;
+            while (repetidor)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Adicionar vértice");
+                Console.WriteLine("2. Adicionar aresta");
+                Console.WriteLine("3. Listar grafo");
+                Console.WriteLine("0. Sair");
+                Console.Write("Escolha uma opção: ");
+                int escolha = Console.ReadLine();
+
+                switch (escolha)
+                {
+                    case 1:
+                        addVertice(grafo);
+                        break;
+                    case 2:
+                        addAresta(grafo);
+                        break;
+                    case 3:
+                        grafo.listar();
+                        break;
+                    case 0:
+                        Console.WriteLine("Saindo...");
+                        repetidor = false;
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
                 }
             }
+        }
+
+        public static bool addVertice(Grafo grafo)
+        {
+            Console.WriteLine("Insira o número do vertice");
+            int vertice = int.Parse(Console.ReadLine());
+            if (grafo.AdicionaVertice(vertice) && vertice > 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Adicionado com sucesso");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Vertice nao existe");
+                return false;
+            }
+            return true;
+        }
+
+        public static bool addAresta(Grafo grafo)
+        {
+            Console.WriteLine("Insira o número do vertice de origem");
+            int verticeOrigem = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o número do vertice de destino");
+            int verticeDestino = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o peso da aresta");
+            double peso = double.Parse(Console.ReadLine());
+
+            if (grafo.AdicionarAresta(verticeOrigem, verticeDestino, peso){
+                Console.Clear();
+                Console.WriteLine("Adicionado com sucesso");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Um ou os dois vértices não existem");
+                return false;
+            }
+            return true;
         }
     }
 }
