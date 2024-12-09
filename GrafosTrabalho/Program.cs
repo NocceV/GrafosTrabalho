0b7f0138dcfea0daa37415908ea5c10ca3f4bb12
Vitor Nocce <noccevitor@gmail.com>
2024-12-08 21:26:03 -0300

    a

--- GrafosTrabalho/Program.cs
+++ GrafosTrabalho/Program.cs
@@ -801,6 +801,7 @@ namespace GrafosTrabalho
         {
             Console.WriteLine("Informe o vértice");
             int vertice = int.Parse(Console.ReadLine());
+            vertice--;
             return vertice;
         }
 
@@ -813,9 +814,11 @@ namespace GrafosTrabalho
             Console.Clear();
             Console.WriteLine("Informe o vértice de origem da aresta");
             int origem = int.Parse(Console.ReadLine());
+            origem--;
 
             Console.WriteLine("Informe o vértice de destino da aresta");
             int destino = int.Parse(Console.ReadLine());
+            destino--;
 
             Console.WriteLine("Informe o peso da aresta");
             int peso = int.Parse(Console.ReadLine());
